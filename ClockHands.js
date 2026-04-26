537066d9dda5b3a0f279c4ed8108d01eb3650dd0
DanielDecker <90106468+DanielDecker@users.noreply.github.com>
2026-04-26 19:49:50 +0200

    inital commit

--- ClockHands.js
+++ ClockHands.js
@@ -1,91 +1,89 @@
 ;(function () {
-  let HourHandWidth   = 2*3, halfHourHandWidth   = HourHandWidth/2;
-  let MinuteHandWidth = 2*2, halfMinuteHandWidth = MinuteHandWidth/2;
+  let two_pi = Math.PI * 2;
+  let pi_half = Math.PI / 2;
 
-  let SecondHandOffset = 10;
+  function drawRotDigit(poly_i, cos_a, sin_a, param) {
+    let poly_rot = [];
 
-  let twoPi  = 2*Math.PI, deg2rad = Math.PI/180;
-  let Pi     = Math.PI;
-  let halfPi = Math.PI/2;
-
-  let sin = Math.sin, cos = Math.cos;
-
-  let HourHandLength  = 0;
-  let HourHandPolygon = new Array(8);
-
-  function prepareHourHandPolygon(newHourHandLength) {
-    if (HourHandLength === newHourHandLength) { return; }
-
-    HourHandLength  = newHourHandLength;
-    HourHandPolygon = [
-      -halfHourHandWidth,halfHourHandWidth,
-      -halfHourHandWidth,halfHourHandWidth-HourHandLength,
-       halfHourHandWidth,halfHourHandWidth-HourHandLength,
-       halfHourHandWidth,halfHourHandWidth,
-    ];
-  }
-
-  let MinuteHandLength  = 0;
-  let MinuteHandPolygon = new Array(8);
-
-  function prepareMinuteHandPolygon(newMinuteHandLength) {
-    if (MinuteHandLength === newMinuteHandLength) { return; }
-
-    MinuteHandLength  = newMinuteHandLength;
-    MinuteHandPolygon = [
-      -halfMinuteHandWidth,halfMinuteHandWidth,
-      -halfMinuteHandWidth,halfMinuteHandWidth-MinuteHandLength,
-       halfMinuteHandWidth,halfMinuteHandWidth-MinuteHandLength,
-       halfMinuteHandWidth,halfMinuteHandWidth,
-    ];
-  }
-
-  let transformedPolygon = new Array(HourHandPolygon.length);
-
-  function transformPolygon (originalPolygon, OriginX,OriginY, Phi) {
-    let sPhi = sin(Phi), cPhi = cos(Phi), x,y;
-
-    for (let i = 0, l = originalPolygon.length; i < l; i+=2) {
-      x = originalPolygon[i];
-      y = originalPolygon[i+1];
-
-      transformedPolygon[i]   = OriginX + x*cPhi + y*sPhi;
-      transformedPolygon[i+1] = OriginY + x*sPhi - y*cPhi;
+    // Rotate every point of the digit
+    for (let j=0; j<poly_i.length; j+=2) {
+      var x = poly_i[j];
+      var y = poly_i[j+1];
+      poly_i[j] = (x * cos_a + y * sin_a) + param.cx;
+      poly_i[j+1] = (-x * sin_a + y * cos_a) + param.cy;
     }
+
+    g.fillPoly(poly_i);
+  }
+
+
+  function drawClockHand(hours, minutes, outerRadius, font_size, param) {
+    // Calculate transformation rotation
+    let rotation;
+    if ( hours == -1 )
+      rotation = two_pi * (15 - minutes) / 60;
+    else
+      rotation = two_pi * (3 - hours - minutes / 60) / 12;
+
+    // Set font to get accuarte result for stringWidth
+    g.setFont('Vector', font_size);
+    // no hours => minute hand
+    let digits = (hours === -1 ? minutes : hours);
+    let separator = ':';
+    let str_width = g.stringWidth(separator + digits);
+
+    // Build clock hand
+    let right_side = ( rotation < pi_half && rotation > -pi_half );
+    // For right hand side clock hand use forward text, e. g. 24:24>
+    // For left hand side clock hand use backward text, e. g. <24:24
+    let text = (right_side ? ('' + digits) : ('<' + digits));
+    for (let i=(str_width*2); i<=outerRadius; i+=str_width)
+      text += separator + digits;
+
+    let x_offset, x_off_rect;
+    if ( right_side ) {
+      text += '>';
+      x_offset = x_off_rect= -8;
+    } else {
+      x_offset = -g.stringWidth(text) + 8;
+      x_off_rect = x_offset +12;
+      rotation -= Math.PI;
+    }
+    // apply half font hight offset, to fix rotation axis
+    var poly = g.getVectorFontPolys(text, {x:x_offset, y:-font_size/2, w:font_size, h:font_size});
+
+    let cos_a = Math.cos(rotation);
+    let sin_a = Math.sin(rotation);
+
+    // draw background rect
+    g.setColor(param.cfg.Background === 'Theme' ? g.theme.bg : param.cfg.Background || '#FFFFFF');
+    let rect_top = -font_size/3 * 1.3;
+    let rect_bottom = font_size/3* 0.9;
+    let rect_length = g.stringWidth(text) - 12;
+    bg_rect = [x_off_rect, rect_top,
+               x_off_rect + rect_length, rect_top,
+               x_off_rect + rect_length, rect_bottom,
+               x_off_rect, rect_bottom];
+    drawRotDigit(bg_rect, cos_a, sin_a, param);
+
+    // draw clock hands with foreground color
+    g.setColor(param.cfg.Foreground === 'Theme' ? g.theme.fg : param.cfg.Foreground || '#000000');
+    for (let i=0; i<poly.length; i++)
+      drawRotDigit(poly[i], cos_a, sin_a, param);
   }
 
   exports.draw = function draw (
-    Settings, CenterX, CenterY, outerRadius, Hours,Minutes,Seconds
-  ) {
-    prepareHourHandPolygon  (outerRadius * 0.5);
-    prepareMinuteHandPolygon(outerRadius * 0.8);
+      Settings, CenterX, CenterY, outerRadius, Hours, Minutes, Seconds
+    ) {
+    let param = {
+      cx : CenterX,
+      cy : CenterY,
+      cfg : Settings
+    };
 
-    let HoursAngle   = (Hours+(Minutes/60))/12 * twoPi - Pi;
-    let MinutesAngle = (Minutes/60)            * twoPi - Pi;
+    drawClockHand(-1, Minutes, outerRadius * 0.9, 16, param);
+    drawClockHand(Hours, Minutes, outerRadius * 0.65, 20, param);
 
-    g.setColor(Settings.Foreground === 'Theme' ? g.theme.fg : Settings.Foreground || '#000000');
-
-    transformPolygon(HourHandPolygon, CenterX,CenterY, HoursAngle);
-    g.fillPoly(transformedPolygon);
-
-    transformPolygon(MinuteHandPolygon, CenterX,CenterY, MinutesAngle);
-    g.fillPoly(transformedPolygon);
-
-    if (Seconds != null) {
-      g.setColor(Settings.Seconds === 'Theme' ? g.theme.fgH : Settings.Seconds || '#FF0000');
-
-      let SecondsAngle = (Seconds/60) * twoPi - Pi;
-
-      let sPhi = Math.sin(SecondsAngle), cPhi = Math.cos(SecondsAngle);
-
-      let SecondHandLength = outerRadius * 0.9;
-      g.drawLine(
-        CenterX + SecondHandOffset*sPhi,
-        CenterY - SecondHandOffset*cPhi,
-        CenterX - SecondHandLength*sPhi,
-        CenterY + SecondHandLength*cPhi
-      );
-    }
   };
 })();
 
